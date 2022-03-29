6b0382e37204211b9fcfbd08e80b77a1e893232c
mavliuda19 <97443166+mavliuda19@users.noreply.github.com>
2022-03-29 16:43:56 +0600

    hw_22

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,13 +1,16 @@
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useHistory } from 'react-router-dom'
 import { authContext } from '../../store/authContext'
 import classes from './MainNavigation.module.css'
 
+
 const MainNavigation = () => {
+	const history = useHistory()
 	const authctx = useContext(authContext)
 
 	const isLoggedIn = authctx.isLoggedIn
 	const logoutHandler = () => {
+		history.replace('/auth')
 		authctx.logout()
 	}
 	return (
